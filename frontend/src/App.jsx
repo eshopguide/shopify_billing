21f777f257143eab8efbe4a9e6eb64e23685067f
Lukas Klappert <lukas@eshop-guide.de>
2025-06-04 18:42:51 +0200

    fix: styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { AppProvider } from "polaris-13";
-import styles from "polaris-13/build/esm/styles.css?raw";
 import ReactShadowRoot from "react-shadow-root";
+import styles from "polaris-13/build/esm/styles.css?raw";
+
 import BillingPage from "./pages/Billing";
 import { BillingProvider } from "./providers/BillingProvider";
 
@@ -8,7 +9,12 @@ export default function Billing(props) {
   return (
     <div>
       <ReactShadowRoot>
-        <style>{styles.replace(":root", ":host")}</style>
+        <style>
+          {styles
+            .replaceAll(":root", ":host")
+            .replaceAll("html", ":host")
+            .replaceAll("body", ":host")}
+        </style>
         <AppProvider>
           <BillingProvider {...props}>
             <BillingPage />

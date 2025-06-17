ab7a4b27ed3066f600d3c16a562a947336ed8ed7
Lukas Klappert <lukas@eshop-guide.de>
2025-06-17 13:23:40 +0200

    fix

--- frontend/src/components/PolarisWrapper.jsx
+++ frontend/src/components/PolarisWrapper.jsx
@@ -1,17 +1,20 @@
 import { AppProvider } from "billing-polaris";
 import ReactShadowRoot from "react-shadow-root";
 import styles from "billing-polaris/build/esm/styles.css?raw";
+import { useMemo } from "react";
 
 export default function PolarisWrapper({ children, ...props }) {
+  const polarisStyles = useMemo(() => {
+    return styles
+      .replaceAll(":root", ":host")
+      .replaceAll("html", ":host")
+      .replaceAll("body", ":host");
+  }, []);
+
   return (
     <div {...props}>
       <ReactShadowRoot>
-        <style>
-          {styles
-            .replaceAll(":root", ":host")
-            .replaceAll("html", ":host")
-            .replaceAll("body", ":host")}
-        </style>
+        <style>{polarisStyles}</style>
         <AppProvider>{children}</AppProvider>
       </ReactShadowRoot>
     </div>
